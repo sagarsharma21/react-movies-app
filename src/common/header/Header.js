54cac7742f18b739663ce0ffd1f8bd52279b000c
sagarsharma21 <007sharmasagar@gmail.com>
2022-10-02 18:52:31 +0530

    Added username and passsword inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -6,6 +6,28 @@ import logo1 from '../../assets/logo1.svg';
 import Modal from 'react-modal';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Input from '@mui/material/Input';
+import { useSlotProps } from '@mui/base';
+
+// const customStyles = {
+//     content : {
+//         import  { Component } from 'react',
+//         import './Header.css',
+//         import Button from '@mui/material/Button';
+//         import logo from '../../assets/logo1.svg';
+//     }
+// };
+
+const TabContainer = function () {
+    return (
+        <Typography component="div" style={{padding:0}}>
+            {useSlotProps.children}
+        </Typography>
+    )
+} 
 
 class Header extends Component {
 
@@ -45,11 +67,30 @@ class Header extends Component {
                     ariaHideApp={false}
                     isOpen={this.state.modalIsOpen}
                     contentLabel="Login"
-                    onRequestClose={this.closeModalHandler}>
+                    onRequestClose={this.closeModalHandler}
+                    // style={customStyles}
+                    >
                     <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        top: '50%',
+                        left: '50%',
+                        bottom: 'auto',
+                        right: 'auto',
+                        marginRight: '-50%',
+                        transform: 'translate(-50%, -50%)'
                         <Tab label="Login" />
                         <Tab label="Register" />    
                     </Tabs>     
+                    <TabContainer>
+                        <FormControl required>
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <Input id="username" type="text" />  
+                        </FormControl>
+
+                        <FormControl required>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" type="password" />
+                        </FormControl>
+                    </TabContainer>
                 </Modal>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Home from './screens/home/Home';
+import 'typeface-roboto';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
