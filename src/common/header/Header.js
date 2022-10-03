3d360e01537553e8baf1e6bad71cf3581c1cddbe
sagarsharma21 <007sharmasagar@gmail.com>
2022-10-03 18:32:06 +0530

    Added PropTypes for TabContainer
    Added a check for state value when displaying login TabContainer

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -11,6 +11,7 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Input from '@mui/material/Input';
 import { useSlotProps } from '@mui/base';
+import PropTypes from 'prop-types';
 
 // const customStyles = {
 //     content: {
@@ -30,7 +31,9 @@ const TabContainer = function (props) {
         </Typography>
     )
 } 
-
+TabContainer.PropTypes = {
+    children: PropTypes.node.isRequired
+}
 class Header extends Component {
 
     constructor(){
@@ -82,6 +85,7 @@ class Header extends Component {
                         <Tab label="Login" />
                         <Tab label="Register" />    
                     </Tabs>     
+                    {this.state.value === 0 &&
                     <TabContainer>
                         <FormControl required>
                             <InputLabel htmlFor="username">Username</InputLabel>
@@ -94,6 +98,7 @@ class Header extends Component {
                         </FormControl>
                         <br></br>
                     </TabContainer>
+                    }
                 </Modal>
             </div>
         )
